a2e4d259cb5f84c2f3693a02b2c29facfc62bc29
Paul Bellamy <paul.a.bellamy@gmail.com>
2012-06-04 14:17:15 +0100

    Implemented Reduce, (I'm not crazy about the differences between Reduce and ReduceFunc)

---
+++ reduce.go
@@ -0,0 +1,56 @@
+// Copyright 2012 Paul Bellamy. All rights reserved.
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
+package pipe
+
+// Implement this interface in your object to pass it to Pipe.Reduce
+type Reducer interface {
+  Reduce(item interface{}) interface{}
+}
+
+// A function which reduces
+type ReduceFunc func(result, item interface{}) interface{}
+
+// Add a transformation to the end of the pipe
+func (p *Pipe) ReduceFunc(initial interface{}, fn ReduceFunc) {
+  p.addStage()
+	go p.reducerHandler(initial, fn, p.length-1)()
+}
+
+// Add a transformation to the end of the pipe
+func (p *Pipe) Reduce(t Reducer) {
+  p.addStage()
+  var pos int = p.length - 1
+  var result interface{}
+  go func() {
+    for {
+      item, ok := <-p.prevChan(pos)
+      if (!ok) {
+        break
+      }
+
+      result = t.Reduce(item)
+    }
+    // Input was closed, send the result
+    p.nextChan(pos) <- result
+    close(p.nextChan(pos))
+  }()
+}
+
+func (p *Pipe) reducerHandler(initial interface{}, fn ReduceFunc, pos int) func() {
+  var result interface{} = initial
+	return func() {
+    for {
+      item, ok := <-p.prevChan(pos)
+      if (!ok) {
+        break
+      }
+
+      result = fn(result, item)
+		}
+    // Input was closed, send the result
+    p.nextChan(pos) <- result
+    close(p.nextChan(pos))
+	}
+}
---
+++ reduce_test.go
@@ -0,0 +1,66 @@
+// Copyright 2012 Paul Bellamy. All rights reserved.
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
+package pipe
+
+import (
+  "testing"
+)
+
+type FakeReducer struct {
+  sum int
+}
+
+// sum the elements
+func (t *FakeReducer) Reduce(item interface{}) interface{} {
+  t.sum += item.(int)
+  return t.sum
+}
+
+func TestReduceFuncPipe(t *testing.T) {
+  in := make(chan interface{}, 5)
+  out := make(chan interface{}, 5)
+  pipe := NewPipe(in, out)
+  pipe.ReduceFunc(0, func(sum, item interface{}) interface{}  {
+    return sum.(int) + item.(int)
+  })
+
+  in <- 5
+  in <- 10
+  in <- 20
+  close(in)
+
+  result, ok := <-out
+  if !ok {
+    t.Fatal("output channel was closed before we retrieved the result")
+  }
+
+  if result.(int) != 35 {
+    t.Fatal("reducing (sum) pipe received 5, 10, and 20 items but output ",result.(int))
+  }
+}
+
+func TestReducePipe(t *testing.T) {
+  in := make(chan interface{}, 10)
+  out := make(chan interface{}, 10)
+  pipe := NewPipe(in, out)
+  pipe.Reduce(&FakeReducer{})
+
+  // Push in some numbers
+  for i := 5; i > 0; i-- {
+    in <- i
+  }
+
+  close(in)
+
+  result, ok := <-out
+  if !ok {
+    t.Fatal("output channel was closed before we retrieved the result")
+  }
+
+  expected := 5 + 4 + 3 + 2 + 1
+  if result.(int) != expected {
+    t.Fatal("reducing (sum) pipe received 5..1 items but output ",result.(int))
+  }
+}
