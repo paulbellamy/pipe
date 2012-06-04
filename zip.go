fef17e15fc9aef26d50696a876a09537b81170c4
Paul Bellamy <paul.a.bellamy@gmail.com>
2012-06-04 14:31:02 +0100

    Added Zip

---
+++ zip.go
@@ -0,0 +1,32 @@
+// Copyright 2012 Paul Bellamy. All rights reserved.
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
+package pipe
+
+// Add a transformation to the end of the pipe
+func (p *Pipe) Zip(other chan interface{}) {
+  p.addStage()
+	go p.zipperHandler(other, p.length-1)()
+}
+
+func (p *Pipe) zipperHandler(other chan interface{}, pos int) func() {
+	return func() {
+    // only send num items
+    for {
+      a, ok := <-p.prevChan(pos)
+      if !ok {
+        break
+      }
+
+      b, ok := <-other
+      if !ok {
+        break
+      }
+
+      p.nextChan(pos) <- []interface{}{a, b}
+		}
+
+    close(p.nextChan(pos))
+	}
+}
---
+++ zip_test.go
@@ -0,0 +1,37 @@
+// Copyright 2012 Paul Bellamy. All rights reserved.
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
+package pipe
+
+import (
+  "testing"
+)
+
+func TestZipPipe(t *testing.T) {
+  in := make(chan interface{}, 5)
+  out := make(chan interface{}, 5)
+  other := make(chan interface{}, 5)
+  pipe := NewPipe(in, out)
+  pipe.Zip(other)
+
+  in <- 5
+  in <- 10
+  in <- 20
+  other <- 6
+  other <- 11
+
+  for i := 1; i <= 2; i++ {
+    result := <-out
+    expected := []int{i*5, (i*5)+1}
+    if len(result.([]interface{})) != len(expected) {
+      t.Fatal("expected channel output to match",expected,"but got",result.([]int))
+    }
+
+    for j := 0; j < len(result.([]interface{})); j++ {
+      if result.([]interface{})[j].(int) != expected[j] {
+        t.Fatal("expected channel output to match",expected,"but got",result.([]interface{}))
+      }
+    }
+  }
+}
