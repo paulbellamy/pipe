156c8eeee1ddb0cab76d56127b1ed687eeff15a2
Paul Bellamy <paul.a.bellamy@gmail.com>
2014-01-11 00:21:25 +0000

    s/2012/2014/g

--- zip.go
+++ zip.go
@@ -1,4 +1,4 @@
-// Copyright 2012 Paul Bellamy. All rights reserved.
+// Copyright 2014 Paul Bellamy. All rights reserved.
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file.
 
