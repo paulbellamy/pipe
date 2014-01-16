a4c43509239065f5de1c80fe97d523c132163a42
Paul Bellamy <paul.a.bellamy@gmail.com>
2014-01-16 20:32:54 +0000

    adding a population example

---
+++ examples/population.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	. "pipe"
+)
+
+type place struct {
+	name       string
+	population int
+}
+
+func statusByPopulation(p place) string {
+  switch {
+  case p.population > 1000000:
+    return "City"
+  case p.population > 5000:
+    return "Town"
+  default:
+    return "Village"
+  }
+}
+
+func main() {
+	places := []place{
+		{"Grantchester", 552},
+		{"Cambridge", 117900},
+		{"Prague", 1188126},
+	}
+	fmt.Println(Map(places, statusByPopulation))
+}
