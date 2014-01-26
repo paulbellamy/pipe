594474a3e6ef386cca978bdbcf261675f624a92b
Paul Bellamy <paul.a.bellamy@gmail.com>
2014-01-26 21:38:06 +0000

    adding docs for FieldByName

--- field_by_name.go
+++ field_by_name.go
@@ -4,6 +4,8 @@ import (
 	"reflect"
 )
 
+// FieldByName is a helper for fetching a named field from a struct. It is
+// useful in conjunction with Map, to retrieve many values at once.
 func FieldByName(name string) func(interface{}) interface{} {
 	return func(record interface{}) interface{} {
 		return reflect.ValueOf(record).FieldByName(name).Interface()
